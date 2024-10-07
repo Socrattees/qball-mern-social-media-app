6bd2b2eedfe8e599763ccb9ad1a9a9e78d29c229
Socrattees <qiniselad@gmail.com>
2024-10-07 21:32:42 +0200

    Add Share.jsx and update colour in Navbar

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./feed.css";
+import Share from "../share/Share";
 
 export default function Feed() {
   return (
     <div className="feed">
-      <div className="feedWrapperr">
-        
+      <div className="feedWrapper">
+        <Share />
       </div>
     </div>
   );
