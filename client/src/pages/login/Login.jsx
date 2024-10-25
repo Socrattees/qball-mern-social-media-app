87a5e02eb69dc11ab75c8e727d218598ad925a65
Socrattees <qiniselad@gmail.com>
2024-10-25 17:29:10 +0200

    Create files for context in regards to user

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./login.css";
 
 export default function Login() {
+  const email = useRef(); //unlike useState, prevents re-renders to save performance
+  const password = useRef(); //also it's designed to directly interacting with DOM elements and not so much on states
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  }
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,14 +19,14 @@ export default function Login() {
             Connect with friends from around the world.
           </span>
         </div>
-        <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" type="email" className="loginInput" />
-            <input placeholder="Password" type="password" className="loginInput" />
+        <div className="loginRight" onSubmit={ handleClick }>
+          <form className="loginBox">
+            <input placeholder="Email" type="email" className="loginInput" ref={ email } required />
+            <input placeholder="Password" type="password" className="loginInput" ref={ password } required />
             <button className="loginButton">Log in</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">Create a New Account</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
